4ad38e0b93aecc1fcb6b27a4b40117b11c5584d9
Cam <camsandybaker@gmail.com>
2021-08-17 07:26:51 -0600

    Finished home page styling and built login page. started stash page

--- server/server.js
+++ server/server.js
@@ -3,20 +3,22 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const users = require('./routes/community/users');
+const { checkToken } = require('./middleware/auth');
 
-const mongoUri=`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
-mongoose.connect(mongoUri,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
-
-
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(bodyParser.json());
+app.use(checkToken);
+app.use('/community/users', users);
 
 const port = process.env.PORT || 3001;
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});

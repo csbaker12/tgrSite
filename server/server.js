bfe08667f6fdf5dcb5e206fc0926ee0007d4ee42
Cam <camsandybaker@gmail.com>
2021-08-18 16:48:35 -0600

    deployment attempt

--- server/server.js
+++ server/server.js
@@ -18,6 +18,15 @@ app.use(bodyParser.json());
 app.use(checkToken);
 app.use('/community/users', users);
 
+app.use(express.static('client/build'));
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
